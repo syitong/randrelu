c69cf8c6c2d4187063923fd7c988c61961446f9c
Yitong Sun <syitong@umich.edu>
2019-04-30 16:18:18 -0400

    get RFparams results

--- depth_sep/RFparams.sh
+++ depth_sep/RFparams.sh
@@ -2,11 +2,11 @@
 
 for N in 20 40 80 160 320 640 1280 2560 5120
 do
-    for trial in 0 1 2 3 4 5 6 7
-    do
-        python experiments.py --N ${N} --trial ${trial} --file eldan-RF-params
-        wait
-    done
+    # for trial in 0 # 1 2 3 4 5 6 7
+    # do
+    #     python experiments.py --N ${N} --trial ${trial} --file eldan-RF-params
+    #     wait
+    # done
     python result_alloc.py --file eldan-RF-params --N ${N}
     wait
 done
--- depth_sep/librf.py
+++ depth_sep/librf.py
@@ -202,10 +202,10 @@ class RF:
         np_init = np.random.choice([-1,1],size=(N,n_outputs))
         logits_init = tf.constant_initializer(np_init,dtype=tf.float32)
         logits = tf.layers.dense(inputs=x,
-            # use_bias = False,
+            use_bias = False,
             kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=
                 self._Lambda),
-            # kernel_initializer=logits_init,
+            kernel_initializer=logits_init,
             units=n_outputs,name='Logits')
         tf.summary.histogram('outer weights',
             self._graph.get_tensor_by_name('Logits/kernel:0'))
