5a3d0d42c4ad80a0a2d23f0a370f51cafd773f9f
Yitong Sun <syitong@spada.eecs.umich.edu>
2019-04-30 14:45:33 -0400

    run NNtest

--- depth_sep/NNtest.sh
+++ depth_sep/NNtest.sh
@@ -1,2 +1,10 @@
-# python experiments.py --dataset eldan --model NN --N 10 --H 2 --trial 0
-python result_alloc.py --dataset eldan --model NN --N 10 --n_epoch 100 --trials 1
+for N in 10 16 24 36 54 78 113 162 231
+do
+    for t in 0 # {0..9}
+    do
+        python experiments.py --dataset eldan --model NN --N ${N} --H 2 --trial ${t}
+        wait
+    done
+    # python result_alloc.py --dataset eldan --model NN --N ${N} --n_epoch 100 --trials 10
+    # wait
+done
