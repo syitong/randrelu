42890151deb8b29f14d52801516650b9df09b7e6
Sun, Yitong <sunny19890224@gmail.com>
2019-04-30 20:29:55 -0400

    allocate 3 layer NN test results

--- depth_sep/NNtest.sh
+++ depth_sep/NNtest.sh
@@ -1,10 +1,10 @@
 for N in 10 16 24 36 54 78 113 162 231
 do
-    for t in 0 # {0..9}
-    do
-        python experiments.py --dataset eldan --model NN --N ${N} --H 2 --trial ${t}
-        wait
-    done
-    # python result_alloc.py --dataset eldan --model NN --N ${N} --n_epoch 100 --trials 10
-    # wait
+    # for t in 0 # {0..9}
+    # do
+    #     python experiments.py --dataset eldan --model NN --N ${N} --H 2 --trial ${t}
+    #     wait
+    # done
+    python result_alloc.py --dataset eldan --model NN --N ${N} --n_epoch 100 --trials 10
+    wait
 done
