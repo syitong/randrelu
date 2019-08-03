2b41f71b06f210033d6ca6f0cbd10d73aed60b05
Yitong Sun <syitong@umich.edu>
2019-08-03 15:30:48 +0800

    ready to run daniely

--- depth_sep/daniely.sh
+++ depth_sep/daniely.sh
@@ -9,7 +9,7 @@ do
       python experiments.py --action screen --N ${N} --trial ${trial} --file daniely-RF-params
       wait
     done
-    python result_alloc.py --action screen --file daniely-RF-params --N ${N} --H 2
+    python result_alloc.py --action screen --file daniely-RF-params --N ${N}
     wait
 done
 
@@ -17,7 +17,7 @@ for N in 12 20 # 31 48 72 105 153 221 317
 do
     for trial in 0 # 1 2 3 4 5 6 7
     do
-      python experiments.py --action screen --N ${N} --trial ${trial} --file daniely-NN-params
+      python experiments.py --action screen --N ${N} --trial ${trial} --file daniely-NN-params --H 2
       wait
     done
     python result_alloc.py --action screen --file daniely-NN-params --N ${N} --H 2
@@ -27,7 +27,7 @@ for N in 20 40 # 80 160 320 640 1280 2560 5120
 do
     for trial in 0 # 1 2 3 4 5 6 7
     do
-      python experiments.py --action screen --N ${N} --trial ${trial} --file daniely-RF-params
+      python experiments.py --action screen --N ${N} --trial ${trial} --file daniely-RF-params --H 1
       wait
     done
     python result_alloc.py --action screen --file daniely-NN-params --N ${N} --H 1
