88bd11ef0f110a602a59b2349083cc6ab422e3f2
Yitong Sun <syitong@umich.edu>
2019-08-07 16:58:47 +0800

    collect 4 dim daniely results of NN

--- depth_sep/NNtest.sh
+++ depth_sep/NNtest.sh
@@ -1,10 +1,11 @@
-for N in 12 20 31 48 72 105 153 221 317
+# for N in 7 11 17 25 37 54 77 110 157
+for N in 20 40 80 160 320 640 1280 2560 5120
 do
-    for trial in 2 # {0..9}
+    for trial in {0..7}
     do
-        python experiments.py --action screen --N ${N} --trial ${trial} --file daniely-NN-params --H 2
+        python experiments.py --action screen --N ${N} --trial ${trial} --file daniely-NN-params --H 1
         wait
     done
-    python result_alloc.py --action screen --file daniely-NN-params --N ${N} --H 2
+    python result_alloc.py --action screen --file daniely-NN-params --N ${N} --H 1
     wait
 done
--- depth_sep/daniely.sh
+++ depth_sep/daniely.sh
@@ -2,34 +2,35 @@
 # For the number of params to be the same with RF models,
 # we set N to be -(d+1)/2 + sqrt((d+1)^2+4(d+1)M)
 # if RF has M nodes and data is d dimensional.
-for N in 20 40 80 160 320 640 1280 2560 5120
-do
-    # for trial in 0 1 2 3 4 5 6 7
-    # do
-    #   python experiments.py --action screen --N ${N} --trial ${trial} --file daniely-RF-params
-    #   wait
-    # done
-    python result_alloc.py --action screen --file daniely-RF-params --N ${N}
-    wait
-done
+# for N in 20 40 80 160 320 640 1280 2560 5120
+# do
+#     for trial in 0 1 2 3 4 5 6 7
+#     do
+#       python experiments.py --action screen --N ${N} --trial ${trial} --file daniely-RF-params
+#       wait
+#     done
+#     python result_alloc.py --action screen --file daniely-RF-params --N ${N}
+#     wait
+# done
 
-for N in 12 20 31 48 72 105 153 221 317
-do
-    # for trial in 0 1 2 3 4 5 6 7
-    # do
-    #   python experiments.py --action screen --N ${N} --trial ${trial} --file daniely-NN-params --H 2
-    #   wait
-    # done
-    python result_alloc.py --action screen --file daniely-NN-params --N ${N} --H 2
-    wait
-done
+# for N in 7 11 17 25 37 54 77 110 157
+# do
+#     for trial in 0 1 2 3 4 5 6 7
+#     do
+#       python experiments.py --action screen --N ${N} --trial ${trial} --file daniely-NN-params --H 2
+#       wait
+#     done
+#     python result_alloc.py --action screen --file daniely-NN-params --N ${N} --H 2
+#     wait
+# done
+
 for N in 20 40 80 160 320 640 1280 2560 5120
 do
-    # for trial in 0 1 2 3 4 5 6 7
-    # do
-    #   python experiments.py --action screen --N ${N} --trial ${trial} --file daniely-RF-params --H 1
-    #   wait
-    # done
+    for trial in 0 1 2 3 4 5 6 7
+    do
+      python experiments.py --action screen --N ${N} --trial ${trial} --file daniely-NN-params --H 1
+      wait
+    done
     python result_alloc.py --action screen --file daniely-NN-params --N ${N} --H 1
     wait
 done
